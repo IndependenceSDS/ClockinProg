50f81a1a077c245dbd5ee2478dc8bc8d11c1650e
IndependenceSDS <1979661141@qq.com>
2021-06-12 21:45:02 +0800

    Add files via upload

---
+++ selfcheck_public.py
@@ -0,0 +1,45 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Mar 20 19:35:22 2021
+
+@author: cgg
+"""
+
+
+from selenium import webdriver
+import selenium
+import time
+import sys
+import socket
+def is_online():
+    try:
+        host=socket.gethostbyname("weixine.ustc.edu.cn")
+        s=socket.create_connection((host,80),2)
+        return True
+    except Exception as e:
+        return False
+while(not is_online()):
+    i=0
+driver = webdriver.Chrome()
+driver.get("https://passport.ustc.edu.cn/login?service=https%3A%2F%2Fweixine.ustc.edu.cn%2F2020%2Fcaslogin")
+time.sleep(1)
+driver.find_element_by_id("username").send_keys("[your student id]")#keyin
+driver.find_element_by_id("password").send_keys("[your password]")#keyin
+driver.find_element_by_id("login").click()
+time.sleep(1)
+driver.find_element_by_id("report-submit-btn").click()
+time.sleep(1)
+driver.get("https://mail.ustc.edu.cn/")
+time.sleep(1)
+driver.find_element_by_id("uid").send_keys("[your mail name]")#keyin
+driver.find_element_by_id("password").send_keys("[your mail password]")#keyin
+js = 'changeDomain(\'mail.ustc.edu.cn\')'#if you are the teacher of USTC,delete "mail." 
+driver.execute_script(js) 
+driver.find_element_by_id("login_button").click()
+time.sleep(1)
+try:
+    driver.find_element_by_id("navNewCount_1")
+except:
+    driver.close()
+    quit()
+driver.find_element_by_id("navFid_1").click()
